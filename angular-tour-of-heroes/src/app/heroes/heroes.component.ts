80d59b9763c2d3e3256dbfbcc0135c03fc2af07d
Naguib Ihab <nick@anideaforanapp.com>
2018-01-01 19:26:57 +1100

    Finished tutorial 4

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 // Metadata
 @Component({
@@ -8,10 +9,12 @@ import { Hero } from '../hero';
 	styleUrls: ['./heroes.component.css'] // the location of the template's css
 })
 export class HeroesComponent implements OnInit {
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+	selectedHero: Hero;
+	heroes = HEROES;
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 
 	constructor() { }
 
