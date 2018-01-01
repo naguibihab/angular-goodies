1b20090536acc1125e642dfc28d813786096c7a2
Naguib Ihab <nick@anideaforanapp.com>
2018-01-01 16:23:39 +1100

    Finished Tutorial 3

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 // Metadata
 @Component({
-  selector: 'app-heroes', // css selector; matches the name of the HTML element that identifies this component within a parent component's template.
-  templateUrl: './heroes.component.html', // The location of the component's template file
-  styleUrls: ['./heroes.component.css'] // the location of the template's css
+	selector: 'app-heroes', // element selector; matches the name of the HTML element that identifies this component within a parent component's template.
+	templateUrl: './heroes.component.html', // The location of the component's template file
+	styleUrls: ['./heroes.component.css'] // the location of the template's css
 })
 export class HeroesComponent implements OnInit {
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
